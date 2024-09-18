96a66236c0a1df6877c9d54c0dc5a8e3f6355370
Tanya Clarisse <2330949@champlaincollege.qc.ca>
2024-09-18 16:18:12 -0400

    Trying to check for user input but it's not working

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -14,9 +14,33 @@ namespace SimpleCalculator
                 // Class to perform actual calculations
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
 
+                Console.WriteLine("Please enter a number");
                 double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+
+
+               /* while()
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+                }
+                */
+
+                Console.WriteLine("Please enter a second number");
                 double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+
+
+                /*while (double.TryParse(value, out value2))
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+                }*/
+
+                Console.WriteLine("Please enter an operation. The valid " +
+                    "operations are: (+,-,*,/ or %) or (add, substract, multiply, divide or modulus");
                 string operation = Console.ReadLine();
+
+                /*
+                 To check operations*/
 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
